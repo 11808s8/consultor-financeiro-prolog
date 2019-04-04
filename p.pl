3a0dd7f23afd11aa8259afcba645a63b755e8406
Adriano Gomes <adrianogsss@gmail.com>
2019-04-03 21:26:45 -0300

    Adição de verificação e mensagens para RESERVA DE EMERGÊNCIA

--- p.pl
+++ p.pl
@@ -14,19 +14,22 @@ menu():-
     write(" 1 - Exibir as aplicações em que um cliente está investindo"),
     nl,
     write("SEGURE ctrl + D para sair."),
+    nl,
     read(X),
     opc(X),
     menu().
 
 opc(X):-
     X = 1,
+    
     write("Digite o nome do cliente. Exemplo: ronaldinho_gaucho."),
+    nl,
     read(C),
     valida_nome_cliente(C),
     exibe_aplicacoes_valores_cliente(C).
     
 valida_nome_cliente(C):-
-    cliente(C,_,_,_,_,_,_,_).
+    cliente(C,_,_,_,_,_,_,_,_).
 
 % cliente(nome,
 %         saldo,
@@ -50,7 +53,8 @@ aplicacao(tesouro_ipca, 5, 12,1000, moderado).
 aplicacao(poupanca, 0, 999,0, nao_recomendado).
 nomes_aplicacoes([cdb, tesouro_selic, tesouro_ipca, poupanca]).
 
-cliente(ronaldinho,5000,1000,5,12,[cdb,poupanca],[1000,20],conservador).
+% cliente(ronaldinho,5000,1000,5,12,[cdb,poupanca],[1000,20],conservador).
+cliente(ronaldinho,5000,1000,5,12,[cdb,poupanca],[1000,20],conservador,500).
 
 verif_pode_aplicar(VALOR_INVEST, PERFIL, [H|T]):-
     
@@ -80,7 +84,7 @@ tamanho_lista([H|T],TAM):-
 
 % nao esta aplicando
 verifica_aplicacoes(C):-
-    cliente(C,S,R, D, P, T, QT, PERF),
+    cliente(C,S,R, D, P, T, QT, PERF,_),
     write(T),
     nl,
     exibe_valores_lista(T),
@@ -90,7 +94,7 @@ verifica_aplicacoes(C):-
     write(TAM).
 
 v_ronaldinho():-
-    cliente(ronaldinho,S,R, D, P, T, QT, PERF),
+    cliente(ronaldinho,S,R, D, P, T, QT, PERF,_),
     valor_minimo_perfil_moderado(S,R,D).
 
 valor_minimo_investimento(SALDO,RENDA,DEP):-
@@ -98,7 +102,7 @@ valor_minimo_investimento(SALDO,RENDA,DEP):-
     S1 is RENDA - MIN,
     S2 is SALDO + S1,
     S2 > 30,
-    S2 < 1000,
+    % S2 < 1000,
     write("Parabéns, você possui saldo o suficiente para investir.").
 
 minimo_dependentes(DEP,MIN):-
@@ -107,8 +111,60 @@ minimo_dependentes(DEP,MIN):-
 % exibe_renda_adequada(C):-
 
 
+verifica_fundo_emergencia(SALDO, RENDA, DEPENDENTES, DESPESA, SOBRA, APLIC, QT_APLIC):-
+    DESPESA_SEIS_MESES is DESPESA * 6,
+    minimo_dependentes(DEPENDENTES, MINDEP),
+    DESPESA_SEIS_MESES_DEP is MINDEP * 6,
+    DESPESA_TOTAL is DESPESA_SEIS_MESES + DESPESA_SEIS_MESES_DEP,
+    soma_aplicacoes([cdb,tesouro_selic],APLIC,QT_APLIC,SOMA), % se falhar, recomendar investir 
+    SOBRA is SOMA - DESPESA_TOTAL,
+    SOMA > DESPESA_TOTAL,
+    write("Parabéns, você possui um fundo de emergência com "),
+    write(SOBRA),
+    write(" de diferença do mínimo que precisaria para sobreviver por seis meses!"). % aqui avisar que pode investir em mais??
+
+verifica_fundo_emergencia(SALDO, RENDA, DEPENDENTES, DESPESA, SOBRA,APLIC, QT_APLIC):-
+    DESPESA_SEIS_MESES is DESPESA * 6,
+    minimo_dependentes(DEPENDENTES, MINDEP),
+    DESPESA_SEIS_MESES_DEP is MINDEP * 6,
+    DESPESA_TOTAL is DESPESA_SEIS_MESES + DESPESA_SEIS_MESES_DEP,
+    soma_aplicacoes([cdb,tesouro_selic],APLIC,QT_APLIC,SOMA), % se falhar, recomendar investir 
+    SOBRA is DESPESA_TOTAL - SOMA,
+    SOMA < DESPESA_TOTAL,
+    write("Que pena! Faltam "),
+    write(SOBRA),
+    write(" reais para você atingir seu fundo de emergência!"). % aqui avisar que pode investir em mais??
+    
+ronal():-
+    cliente(ronaldinho,SALDO, R_A, DEP, PRAZ, APLIC, QT_APLIC, PERF, DESP),
+    % pega_aplicacao_valor(cdb, APLIC, QT_APLIC, A, V),
+    % write(A),
+    % nl,
+    % write(V),
+    % soma_aplicacoes([cdb,tesouro_selic],APLIC,QT_APLIC,SOMA),
+    % nl,
+    verifica_fundo_emergencia(SALDO, R_A, DEP, DESP, SOBRA, APLIC, QT_APLIC).
+    % write(SOMA).
+
+soma_aplicacoes([],_,_,0).
+soma_aplicacoes([H|T],A,QT,SOMA):-
+    pega_aplicacao_valor(H,A,QT,_,VAL),
+    soma_aplicacoes(T,A,QT,S1),
+    SOMA is S1 + VAL.
+
+pega_aplicacao_valor(_, [], [],_,0).
+pega_aplicacao_valor(A, [A], [A1],A,A1).
+pega_aplicacao_valor(A, [H|T], [H1|T1], APLIC, VAL):-
+    A = H,
+    APLIC = H,
+    VAL is H1.
+pega_aplicacao_valor(A, [H|T], [H1|T1], APLIC, VAL):-
+    pega_aplicacao_valor(A, T, T1, APLIC, VAL).
+
+
+
 exibe_aplicacoes_valores_cliente(C):-
-    cliente(C,_,_,_,_, T, QT,_),
+    cliente(C,_,_,_,_, T, QT,_,_),
     write("Você está investindo em: "),
     nl,
     exibe_aplicacao_valor(T,QT).
